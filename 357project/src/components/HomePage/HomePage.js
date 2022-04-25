46ca2b747fbbf590a044c718f99bbb8532338f5f
justpino7 <nicopino_7@hotmail.com>
2022-04-25 15:48:47 -0400

    cards displayed correctly

--- 357project/src/components/HomePage/HomePage.js
+++ 357project/src/components/HomePage/HomePage.js
@@ -2,22 +2,44 @@ import React from 'react'
 import FoodCard from '../FoodCard'
 import FoodList from '../FoodList'
 
+import './HomePage.css';
+
 export default function HomePage() {
   return (
     <div className='HomePageWrapper'>
-        <div className='HomePageDashboardWrapper'>
-            <div className='HomePageDashboard'>
-                HomePage
-                <div className='LeftoversList'>
-                    Leftovers List
+        <div className='HomePageHeader'>
+            <h1>Overview</h1>
+        </div>
+            <div className="CardContainer">
+                <div className='Card'>
                     <FoodCard/>
-                    <FoodList/>
                 </div>
-                <div className='WeeklyCalendar'>
-                    Weekly Calendar
+                <div className='Card'>
+                    <FoodCard/>
+                </div>
+                <div className='Card'>
+                    <FoodCard/>
+                </div>
+                <div className='Card'>
+                    <FoodCard/>
+                </div>
+                <div className='Card'>
+                    <FoodCard/>
+                </div>
+                <div className='Card'>
+                    <FoodCard/>
+                </div>
+                <div className='Card'>
+                    <FoodCard/>
                 </div>
             </div>
-        </div>
+            <div className='LeftoversList'>
+                Leftovers List
+                <FoodList/>
+            </div>
+            <div className='WeeklyCalendar'>
+                Weekly Calendar
+            </div>
     </div>
   )
 }
