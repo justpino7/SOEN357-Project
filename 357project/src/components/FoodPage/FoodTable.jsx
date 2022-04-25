65811111941511f4abf0f1ea7af83ef6a6fa2d1d
Jeremy-Vo-Buu <vobuujeremy@hotmail.com>
2022-04-25 00:00:19 -0400

    Changed Food Table

--- 357project/src/components/FoodPage/FoodTable.jsx
+++ 357project/src/components/FoodPage/FoodTable.jsx
@@ -1,48 +1,115 @@
 import * as React from 'react';
+import Paper from '@mui/material/Paper';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
 
-function createData(
-    name,
-    expiredCount
-  ) {
-    return { name, expiredCount };
-  }
+const columns = [
+  { id: 'name', label: 'Name', minWidth: 170 },
+  {
+    id: 'expiredCount',
+    label: 'expiredCount',
+    minWidth: 170,
+    align: 'right',
+    format: (value) => value.toLocaleString('en-US'),
+  },
   
-  const rows = [
-    createData('Blueberries',10),
-    createData('Ice cream sandwich', 8),
-    createData('Eclair', 3),
-    createData('Cupcake', 1),
-    createData('Gingerbread', 0),
-  ];
+];
 
-const FoodTable = () => {
-    return (
-        <TableContainer component={Paper} className="FoodTable">
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+function createData(name, expiredCount) {
+  return { name, expiredCount };
+}
+
+const rows = [
+  createData('Avocado', 27),  
+  createData('Strawberries', 24),   
+  createData('Bread', 21),   
+  createData('Salsa', 21),   
+  createData('Milk', 16),   
+  createData('Apple', 16),   
+  createData('Mushroom', 12),   
+  createData('Leftover Spaghetti', 12),   
+  createData('Blueberries', 10),   
+  createData('Kiwi', 7),  
+  createData('Cake', 6), 
+  createData('Mango', 2),  
+  createData('Poptart', 0), 
+  createData('Peanut Butter', 0),
+  createData('Brownies', 0),   
+  createData('Banana', 0),   
+  createData('Orange', 0),   
+  createData('Tangerine', 0),   
+  createData('Leftover Boustan', 0),   
+  createData('Yogurt', 0),
+  createData('Ham', 0),
+  createData('Ribs', 0),
+];
+
+export default function StickyHeadTable() {
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
+  return (
+    <Paper sx={{ width: '100%', overflow: 'hidden' }}>
+      <TableContainer sx={{ maxHeight: 350 }}>
+        <Table stickyHeader aria-label="sticky table">
           <TableHead>
             <TableRow>
-              <TableCell className="tableCell">Food</TableCell>
-              <TableCell className="tableCell">Amount of Times the Food Expired</TableCell>
+              {columns.map((column) => (
+                <TableCell
+                  key={column.id}
+                  align={column.align}
+                  style={{ minWidth: column.minWidth }}
+                >
+                  {column.label}
+                </TableCell>
+              ))}
             </TableRow>
           </TableHead>
           <TableBody>
-            {rows.map((row) => (
-              <TableRow key={row.name}>
-                <TableCell className="tableCell"> {row.name}</TableCell>
-                <TableCell className="tableCell">{row.expiredCount}</TableCell>
-              </TableRow>
-            ))}
+            {rows
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((row) => {
+                return (
+                  <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                    {columns.map((column) => {
+                      const value = row[column.id];
+                      return (
+                        <TableCell key={column.id} align={column.align}>
+                          {column.format && typeof value === 'number'
+                            ? column.format(value)
+                            : value}
+                        </TableCell>
+                      );
+                    })}
+                  </TableRow>
+                );
+              })}
           </TableBody>
         </Table>
       </TableContainer>
-    );
-};
-
-export default FoodTable
+      <TablePagination
+        rowsPerPageOptions={[10, 25, 100]}
+        component="div"
+        count={rows.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </Paper>
+  );
+}
