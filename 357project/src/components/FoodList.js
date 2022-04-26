390d0c7d3dbdf6f7b7d8cc5a2c86ea255faa6bff
justpino7 <nicopino_7@hotmail.com>
2022-04-26 02:19:54 -0400

    Calendar css

--- 357project/src/components/FoodList.js
+++ 357project/src/components/FoodList.js
@@ -24,30 +24,32 @@ const rows = [
 
 export default function DenseTable() {
   return (
-    <TableContainer className='Table' component={Paper}>
-      <Table  size="small" aria-label="a dense table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Expiration Date</TableCell>
-            <TableCell align="right">Food</TableCell>
-            <TableCell align="right">Value</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              key={row.name}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.portion}</TableCell>
-              <TableCell align="right">{row.expiresIn}</TableCell>
+    <div className='TableWrapper'>
+      <TableContainer className='Table' component={Paper}>
+        <Table  size="small" aria-label="a dense table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Expiration Date</TableCell>
+              <TableCell align="right">Food</TableCell>
+              <TableCell align="right">Value</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow
+                key={row.name}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {row.name}
+                </TableCell>
+                <TableCell align="right">{row.portion}</TableCell>
+                <TableCell align="right">{row.expiresIn}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>      
+    </div>
   );
 }
